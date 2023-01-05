20da231efdebc1205ab1af376d6aed52d7512c02
Mikhail Katsman <kry3er@gmail.com>
2023-01-05 18:45:06 +0000

    changed img loading process

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ function init() {
     if (document.querySelector('.portfolio-grid')) {
         console.log('grid detected');
 
+        //load imgs
+        for (let i = 1; i <= 32; i++) {
+            let imgPath = `img/grid-img/${i}.webp`;
+            let gridItem = document.getElementById(`grid-item-${i}`);
+            gridItem.style.setProperty('background-image', `url(${imgPath})`);
+        }
+
+        console.log('imgs loaded')
+
+        // calculate viewport width 
+        // for grid element positions
         const vw = Math.max(
             document.documentElement.clientWidth || 0, 
             window.innerWidth || 0
@@ -21,11 +32,14 @@ function init() {
             } else return 300;
         }
 
+        // initialise Minimasonry plugin with 
+        // appropriate grid-item widths
         var masonry = new MiniMasonry({
             container: '.portfolio-grid',
             baseWidth: itemWidth,
             gutter: 3,
-            surroundingGutter: false
+            surroundingGutter: false,
+            wedge: false
         });
 
         console.log('masonry loaded!');
