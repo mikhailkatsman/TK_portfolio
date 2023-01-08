dd6eadb20d4d9729f94ee736ddc36f6c17eb18a9
Mikhail Katsman <kry3er@gmail.com>
2023-01-08 12:41:33 +0000

    Refactored index.js

--- index.js
+++ index.js
@@ -1,77 +1,85 @@
 // Initialize Swup and listen to page transition events
 const swup = new Swup();
-console.log('Swup initialized');
-console.log(window.location.pathname);
-
-// Run init once on page load
-init();
-manipulateHeader();
-
-// Run init when swup transition occurs, to detect for grids
-document.addEventListener('swup:contentReplaced', init);
 document.addEventListener('swup:animationInStart', manipulateHeader);
 document.addEventListener('swup:popState', manipulateHeader);
-
-function init() {
-    if (window.location.pathname == '/portfolio.html') {
+document.addEventListener('swup:contentReplaced', function() {
+    if (window.location.pathname.includes('/portfolio.html')) {
         console.log('portfolio page detected');
-
-        // calculate viewport width 
-        // for grid element positions
-        const vw = Math.max(
-            document.documentElement.clientWidth || 0, 
-            window.innerWidth || 0
-        );
-
-        let itemWidth = calcWidth(vw);
-
-        function calcWidth(vw) {
-            if (vw < 600) {
-                return 90;
-            } else return 300;
-        }
-
-        // initialise Minimasonry plugin with 
-        // appropriate grid-item widths
-        var masonry = new MiniMasonry({
-            container: '.portfolio-grid',
-            baseWidth: itemWidth,
-            gutter: 3,
-            surroundingGutter: false,
-        });
-
-        console.log('masonry grid initialized');
-
-        //load imgs
-        for (let i = 1; i <= 32; i++) {
-            let imgPath = `img/grid-img/${i}.webp`;
-            let gridItem = document.getElementById(`grid-item-${i}`);
-            let itemSpan = document.getElementById(`item-span-${i}`);
-
-            let preloaderImg = document.createElement('img');
-            preloaderImg.src = imgPath;
-            
-            preloaderImg.addEventListener('load', function() {
-                gridItem.style.backgroundImage = `url(${imgPath})`;
-                itemSpan.style.opacity = '0';
-                preloaderImg = null;
-            });
-        }
-
-        console.log('imgs loaded')
+        initMasonry();
+        loadGridImgs();
     }
+});
 
-    else {
-        console.log('no grid detected');
-    }
+// Run functions once on page reload
+manipulateHeader();
+initMasonry();
+loadGridImgs();
+
+// initialise Minimasonry plugin
+function initMasonry() {
+    var masonry = new MiniMasonry({
+        container: '.portfolio-grid',
+        baseWidth: calcItemWidth(),
+        gutter: 3,
+        surroundingGutter: false,
+    });
+
+    console.log('masonry grid initialized');
 }
 
-// Show header if page is not /index.html
-function manipulateHeader () {
+// Show header if page is not '/index.html'
+// And hide header if page is '/index.html' or '/'
+function manipulateHeader() {
     document.getElementById('header')
         .classList.toggle(
             'header--active', 
             !(window.location.pathname.includes('/index.html') 
             || window.location.pathname === '/')
     );
+}
+
+// Load image data into the grid
+function loadGridImgs() {
+    for (let i = 1; i <= 32; i++) {
+        let imgPath = `img/grid-img/${i}.webp`;
+        let gridItem = document.getElementById(`grid-item-${i}`);
+        let itemSpan = document.getElementById(`item-span-${i}`);
+
+        let preloaderImg = document.createElement('img');
+        preloaderImg.src = imgPath;
+        
+        preloaderImg.addEventListener('load', function() {
+            gridItem.style.backgroundImage = `url(${imgPath})`;
+            itemSpan.style.opacity = '0';
+            preloaderImg = null;
+        });
+    }
+
+    console.log('imgs loaded');
+}
+
+function calcItemWidth() {
+    // calculate viewport width 
+    const vw = Math.max(
+        document.documentElement.clientWidth || 0, 
+        window.innerWidth || 0
+    );
+
+    // calculate grid item width
+    let itemWidth;
+    switch (true) {
+        case vw < 430:
+            itemWidth = 90;
+            break;
+        case vw < 600:
+            itemWidth = 130;
+            break;
+        case vw < 900:
+            itemWidth = 200;
+            break;
+        default:
+            itemWidth = 300;
+    }
+
+    return itemWidth;
 }
