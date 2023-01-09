e7c9e4c1469c9f66148a6cb59cc25c23570fcdab
Mikhail Katsman <kry3er@gmail.com>
2023-01-09 15:49:34 +0000

    implemented mobile nav button

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-// Initialize Swup and listen to page transition events
+// Initialize Swup and listen to transition events
 const swup = new Swup();
 document.addEventListener('swup:animationInStart', manipulateHeader);
 document.addEventListener('swup:popState', manipulateHeader);
-document.addEventListener('swup:contentReplaced', function() {
-    if (window.location.pathname.includes('/portfolio.html')) {
-        console.log('portfolio page detected');
-        initMasonry();
-        loadGridImgs();
-    }
-});
+document.addEventListener('swup:contentReplaced', initMasonry);
 
 // Run functions once on page reload
 manipulateHeader();
 initMasonry();
-loadGridImgs();
 
 // initialise Minimasonry plugin
 function initMasonry() {
+    if(!document.querySelector('.portfolio-grid')){
+        return;
+    }
+
+    console.log('portfolio grid detected');
     var masonry = new MiniMasonry({
         container: '.portfolio-grid',
         baseWidth: calcItemWidth(),
@@ -24,7 +22,9 @@ function initMasonry() {
         surroundingGutter: false,
     });
 
-    console.log('masonry grid initialized');
+    console.log('MiniMasonry initialized');
+
+    loadGridImgs();
 }
 
 // Show header if page is not '/index.html'
@@ -36,26 +36,6 @@ function manipulateHeader() {
             !(window.location.pathname.includes('/index.html') 
             || window.location.pathname === '/')
     );
-}
-
-// Load image data into the grid
-function loadGridImgs() {
-    for (let i = 1; i <= 32; i++) {
-        let imgPath = `img/grid-img/${i}.webp`;
-        let gridItem = document.getElementById(`grid-item-${i}`);
-        let itemSpan = document.getElementById(`item-span-${i}`);
-
-        let preloaderImg = document.createElement('img');
-        preloaderImg.src = imgPath;
-        
-        preloaderImg.addEventListener('load', function() {
-            gridItem.style.backgroundImage = `url(${imgPath})`;
-            itemSpan.style.opacity = '0';
-            preloaderImg = null;
-        });
-    }
-
-    console.log('imgs loaded');
 }
 
 function calcItemWidth() {
@@ -83,3 +63,31 @@ function calcItemWidth() {
 
     return itemWidth;
 }
+
+// Load image data into the grid
+function loadGridImgs() {
+    for (let i = 1; i <= 32; i++) {
+        let imgPath = `img/grid-img/${i}.webp`;
+        let gridItem = document.getElementById(`grid-item-${i}`);
+        let itemSpan = document.getElementById(`item-span-${i}`);
+
+        let preloaderImg = document.createElement('img');
+        preloaderImg.src = imgPath;
+        
+        preloaderImg.addEventListener('load', function() {
+            gridItem.style.backgroundImage = `url(${imgPath})`;
+            itemSpan.style.opacity = '0';
+            preloaderImg = null;
+        });
+    }
+
+    console.log('imgs loaded');
+}
+
+// Nav Button operation
+const btnNavElement = document.querySelector(".btn-mobile-nav");
+const headerElement = document.querySelector(".header");
+
+btnNavElement.addEventListener("click", function () {
+  headerElement.classList.toggle("nav-open");
+});
